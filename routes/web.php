9e91cb9bec5bac4c8ff2f323727e011656ae6ae2
RiGun-K <1234suho@naver.com>
2021-07-06 14:09:30 +0900

    팀 프로젝트 웹 예시

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/posts/team', function () {
+    return view('posts.team');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
