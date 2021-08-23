30e5587709194212e16986f9081193bd6f982a21
RiGun-K <1234suho@naver.com>
2021-08-23 17:31:56 +0900

    2021/08/23

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>안녕하세요. 환영합니다.</title>
+    <title>환영합니다.</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
